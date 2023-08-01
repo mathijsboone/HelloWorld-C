1a083db53277c4d1a4ce10a989dae70a637af59f
boone <boone@LAPTOP-FGTERJRP>
2023-08-01 16:04:26 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            int y = x + 3;
+            string answerPhrase = "The answer is ";
+            Console.WriteLine("Hello World");
+            Console.WriteLine(answerPhrase + y);
+            Console.ReadLine();
+        }
+    }
+}
+
